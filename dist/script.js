b50f718deefc7dbea13315be6fb67d02a862b037
Joe <willievaughn1004@gmail.com>
2023-07-19 19:57:36 -0400

    Add necessary html and scss

--- dist/script.js
+++ dist/script.js
@@ -1,13 +1,21 @@
+let myLibrary = [];
+
+
 function Book(name, author, pages, read) {
     this.name = name;
     this.author = author;
     this.pages = pages;
     this.read = read;
+
     this.info = function() {
         return `${name} by ${author}, ${pages}, ${read}`
     }
+
+    this.readStatus = function() {
+        return
+    }
 }
 
-const boobBook = new Book("Getting Bitches", "Willie Vaughn", 345, "I read it");
+function addBookToLibrary() {
 
-console.log(boobBook.info())
+}
